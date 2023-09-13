b86021e91ad12921c4f3dd5e5e1b9173630d1d0a
Dmytro Savych <sava.savych@gmail.com>
2023-09-13 19:50:54 +0300

    add RandomQuote

--- src/components/RandomQuote/RandomQuote.jsx
+++ src/components/RandomQuote/RandomQuote.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
 import './RandomQuote.css'
-import twitter from '../assets/twitter.png'
-import reload from '../assets/reload.png'
+import twitter from '../assets/twitterIcon.png'
+import reload from '../assets/reloadIcon.png'
 
 const RandomQuote = () => {
+
+    let quotes = [];
+
+    async function loadQuotes(){
+        const response = await fetch("https://type.fit/api/quotes");
+        quotes = await response.json();
+
+    }
+
+
     const [quote, setQuote] = useState({
         text: "Difficult increase the nearer",
         author: "Johann Wolfgang von Goethe"
     });
+
+    const random = () => {
+        const select = quotes[Math.floor(Math.random()*quotes.length)]
+        setQuote(select);
+    }
+
+    loadQuotes();
+
     return (
         <div className='container'>
             <div className='quote'>{quote.text}</div>
@@ -15,8 +33,8 @@ const RandomQuote = () => {
                 <div className='line'></div>
                 <div className='bottom'>
                     <div className='author'>{quote.author}</div>
-                    <div className={icons}>
-                        <img src={reload} alt=""/>
+                    <div className='icons'>
+                        <img src={reload} onClick={()=>{random()}} alt="" />
                         <img src={twitter} alt=""/>
                     </div>
                 </div>
